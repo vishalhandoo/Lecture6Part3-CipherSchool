f8ceaeb6f733504dd1252eb095290be8914c2bc1
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-13 18:23:45 +0530

    Add files via upload

---
+++ Lecture6part3.cpp
@@ -0,0 +1,18 @@
+//while loop
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int i=1;
+    int input;
+    while(i<101){
+        cin>>input;
+        if(input==65){
+            cout<<"You have gussed it right";
+            break;
+        }
+    }
+
+    return 0;
+}
